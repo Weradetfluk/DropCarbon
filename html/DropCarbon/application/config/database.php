b3f7b6788352484534558dd621f18fc12dc3ba84
unknown <kittisak_ndp@outlook.com>
2021-07-19 23:18:38 +0700

    set up env for develop

--- html/DropCarbon/application/config/database.php
+++ html/DropCarbon/application/config/database.php
@@ -70,15 +70,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'DCB';
 $query_builder = TRUE;
 
-$db['default'] = array(
+$db['DCB'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'db',
+	'username' => 'DCB@root',
+	'password' => 'DCBTeam@2021',
+	'database' => 'DCBSystem',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
