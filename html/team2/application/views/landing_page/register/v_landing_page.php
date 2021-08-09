59f8b9179096d71bce74fb414abd4979522a96e7
Naaka Punparich <62160082@go.buu.ac.th>
2021-08-09 22:57:46 +0700

    edit Landing page

--- html/team2/application/views/landing_page/register/v_landing_page.php
+++ html/team2/application/views/landing_page/register/v_landing_page.php
@@ -1,8 +1,27 @@
 <!-- Make by : Naaka Punparich 62160082 -->
 <!DOCTYPE html>
 <html lang="en">
+
 <!-- เริ่ม CSS -->
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Chonburi&display=swap');
+
+    p{
+        padding-bottom: 1%;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        font-family: "Chonburi" !important;
+        text-transform: capitalize;
+        color: #4682B4;
+        ;
+    }
+
     #card1 {
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
         transition: 0.3s;
@@ -24,31 +43,21 @@
 
     #padding {
         padding-right: 2%;
-        padding-bottom: 1%;
+        padding-bottom: 1.3%;
     }
 
     .myButton1 {
-        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-        background: linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
         background-color: #ffffff;
-        border-radius: 6px;
         border: 1px solid #dcdcdc;
         display: inline-block;
         cursor: pointer;
-        color: #666666;
+        color: #DC143C;
         font-family: Arial;
         font-size: 15px;
         font-weight: bold;
         padding: 6px 24px;
         text-decoration: none;
         text-shadow: 0px 1px 0px #ffffff;
-    }
-
-    .myButton1:hover {
-        background: linear-gradient(to bottom, #f6f6f6 5%, #ffffff 100%);
-        background-color: #f6f6f6;
-        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-        transform: scale(1.05);
     }
 
     .myButton1:active {
@@ -57,14 +66,45 @@
     }
 
     .myButton {
-        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-        background: linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
         background-color: #ffffff;
-        border-radius: 6px;
-        border: 1px solid #dcdcdc;
+        border: 0px solid #dcdcdc;
         display: inline-block;
         cursor: pointer;
         color: #666666;
+        font-family: Arial;
+        font-size: 16px;
+        font-weight: bold;
+        padding: 6px 24px;
+        text-decoration: none;
+    }
+
+    .myButton:hover {
+        color: #0080c0;
+    }
+
+    .myButton:active {
+        position: relative;
+        top: 1px;
+        box-shadow: inset 0 -5px 0 #fed602;
+    }
+
+    .nav-item>.active {
+        box-shadow: inset 0 -5px 0 #fed602;
+    }
+
+    .myButton:focus {
+        outline: -webkit-focus-ring-color auto 0px;
+    }
+
+    .myButtonSeeAll {
+        /* box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2); */
+        background: linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
+        background-color: transparent;
+        border-radius: 2px;
+        border: 2px solid #004080;
+        display: inline-block;
+        cursor: pointer;
+        color: #004080;
         font-family: Arial;
         font-size: 15px;
         font-weight: bold;
@@ -73,16 +113,50 @@
         text-shadow: 0px 1px 0px #ffffff;
     }
 
-    .myButton:hover {
+    .myButtonSeeAll:hover {
         background: linear-gradient(to bottom, #f6f6f6 5%, #ffffff 100%);
         background-color: #f6f6f6;
         box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-        transform: scale(1.2);
+        border: 2px solid #f6f6f6;
+        color: #004080;
+        transition: 0.5s;
     }
 
-    .myButton:active {
+    .card-info .card-date {
+        float: left;
+        font-size: 17px;
+    }
+
+    .card-info .card-view {
+        float: right;
+        font-size: 14px;
+    }
+
+    .top-location {
         position: relative;
-        top: 1px;
+    }
+
+    .top-location:hover {
+        border: 5px solid #ffff00;
+        transition: all .2s ease-out;
+    }
+
+    .top-location-overley {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 50%;
+        background-image: linear-gradient(0deg, rgba(0, 0, 0, .75), transparent);
+    }
+
+    .top-location-name {
+        position: absolute;
+        bottom: 20px;
+        left: 30px;
+        z-index: 20;
+        color: #fff;
+
     }
 </style>
 <!-- สิ้นสุด CSS -->
@@ -101,19 +175,19 @@
             <div class="carousel-item active">
                 <div class="row">
                     <!-- รูปที่ 1 ของ Banner  -->
-                    <img class="img-fluid" src="<?php echo base_url() . 'assets/templete/picture' ?>/./banner2.jpg" alt="">
+                    <img class="img-fluid" src="<?php echo base_url() . 'assets/templete/picture' ?>/./banner7.jpg" alt="">
                 </div>
             </div>
             <div class="carousel-item">
                 <div class="row">
                     <!-- รูปที่ 2 ของ Banner -->
-                    <img class="img-fluid" src="<?php echo base_url() . 'assets/templete/picture' ?>/./banner3.jpg" alt="">
+                    <img class="img-fluid" src="<?php echo base_url() . 'assets/templete/picture' ?>/./banner6.jpg" alt="">
                 </div>
             </div>
             <div class="carousel-item">
                 <div class="row">
                     <!-- รูปที่ 3 ของ Banner -->
-                    <img class="img-fluid" src="<?php echo base_url() . 'assets/templete/picture' ?>/./banner1.jpg" alt="">
+                    <img class="img-fluid" src="<?php echo base_url() . 'assets/templete/picture' ?>/./banner5.jpg" alt="">
                 </div>
             </div>
         </div>
@@ -130,25 +204,25 @@
     </div>
     <!-- สิ้นสุด Banner -->
 
-    <!-- เริ่มสีพื้นหลัง bg-light -->
-    <section class="bg-light">
+    <!-- เริ่มสีพื้นหลัง bg-white -->
+    <section class="bg-white">
 
         <!-- เริ่มกิจกรรมล่าสุด -->
         <div class="container py-5">
 
             <!-- หัวข้อ กิจกรรมล่าสุด -->
-            <h1 class="h1" style="padding-bottom: 2%">กิจกรรมล่าสุด</h1>
+            <h1 class="h1" style="padding-bottom: 1%; padding-top: 1%;">กิจกรรมล่าสุด</h1>
 
             <!-- ตัวเปลี่ยนข่าวสารของ กิจกรรมล่าสุด -->
             <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                <li class="nav-item" role="presentation" id="padding">
-                    <button class="myButton" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">10 ม.ค. 64</button>
+                <li class="nav-item" role="presentation">
+                    <button class="myButton active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">10 ก.ค. 64</button>
                 </li>
-                <li class="nav-item" role="presentation" id="padding">
-                    <button class="myButton" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">11 ม.ค. 64</button>
+                <li class="nav-item" role="presentation">
+                    <button class="myButton" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">11 ก.ค. 64</button>
                 </li>
-                <li class="nav-item" role="presentation" id="padding">
-                    <button class="myButton" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">12 ม.ค. 64</button>
+                <li class="nav-item" role="presentation">
+                    <button class="myButton" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">12 ก.ค. 64</button>
                 </li>
             </ul>
 
@@ -160,25 +234,25 @@
 
                     <!-- ตัวกล่องข้อความข่าวสารที่ 1 -->
                     <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p>ระบบแนะนำการท่องเที่ยวเชิงอนุรักษ์สิ่งแวดล้อม (Drop Carbon System) เป็นระบบแนะนำการท่องเที่ยวแบบออนไลน์</p>
                         <div class="col-md-auto" align="right" id="padding">
-                            <a type="button" class="myButton1" href="#">อ่านต่อ</a>
+                            <a type="button" class="myButton1" href="#">อ่านต่อ >></a>
                         </div>
                     </div>
 
                     <!-- ตัวกล่องข้อความข่าวสารที่ 2 -->
                     <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>อำเภอเมืองชลบุรีจึงได้มีการริเริ่มแนวคิดหนึ่งคือการท่องเที่ยวแบบ Low carbon Tourism ซึ่งเป็นกิจกรรมท่องเที่ยวที่เป็นทางเลือกในการช่วยลดคาร์บอนให้น้อยลง </p>
                         <div class="col-md-auto" align="right" id="padding">
-                            <a type="button" class="myButton1" href="#">อ่านต่อ</a>
+                            <a type="button" class="myButton1" href="#">อ่านต่อ >></a>
                         </div>
                     </div>
 
                     <!-- ตัวกล่องข้อความข่าวสารที่ 3 -->
                     <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                        <p>ระบบแนะนำการท่องเที่ยวเชิงอนุรักษ์สิ่งแวดล้อม (Drop Carbon System) </p>
                         <div class="col-md-auto" align="right" id="padding">
-                            <a type="button" class="myButton1" href="#">อ่านต่อ</a>
+                            <a type="button" class="myButton1" href="#">อ่านต่อ >></a>
                         </div>
                     </div>
 
@@ -201,12 +275,12 @@
 
                         <!-- รูปในการ์ดที่ 1 -->
                         <a href="#">
-                            <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./gym.jpg" class="card-img-top" alt="...">
+                            <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./activity1.jpg" class="card-img-top" alt="...">
                         </a>
                         <!-- ข้อความในการ์ดที่ 1 -->
                         <div class="card-body">
-                            <a href="#" class="h2 text-decoration-none text-dark">ออกกำลังกาย</a>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.</p>
+                            <a href="#" class="h2 text-decoration-none text-dark">เก็บขยะริมหาด</a>
+                            <p class="card-text">จากปัญหาสิ่งแวดล้อมชายฝั่งทะเลในปัจจุบัน ได้เกิดปัญหาขยะซึ่งเป็นมลพิษทางทะเลส่งผลกระทบต่อ...</p>
                         </div>
 
                     </div>
@@ -220,12 +294,12 @@
 
                         <!-- รูปในการ์ดที่ 2 -->
                         <a href="#">
-                            <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./manrun.jpg" class="card-img-top" alt="...">
+                            <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./activity2.jpg" class="card-img-top" alt="...">
                         </a>
                         <!-- ข้อความในการ์ดที่ 2 -->
                         <div class="card-body">
-                            <a href="#" class="h2 text-decoration-none text-dark">วิ่งออกกำลังกาย</a>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.</p>
+                            <a href="#" class="h2 text-decoration-none text-dark">ปลูกป่าชายเลน</a>
+                            <p class="card-text">จากการทำงานร่วมกันกับกลุ่มอนุรักษ์ต่างๆในประเทศไทย โดยเฉพาะกลุ่มอนุรักษ์ใน จ.ชลบุรี...</p>
                         </div>
 
                     </div>
@@ -239,12 +313,12 @@
 
                         <!-- รูปในการ์ดที่ 3 -->
                         <a href="#">
-                            <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./swimmer.jpg" class="card-img-top" alt="...">
+                            <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./activity3.jpg" class="card-img-top" alt="...">
                         </a>
                         <!-- ข้อความในการ์ดที่ 3 -->
                         <div class="card-body">
-                            <a href="#" class="h2 text-decoration-none text-dark">ว่ายน้ำ</a>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.</p>
+                            <a href="#" class="h2 text-decoration-none text-dark">วิ่งชมธรรมชาติ</a>
+                            <p class="card-text">ขอเชิญนักวิ่งมาสัมผัสกับธรรมชาติ สูดอากาศโอโซนให้เต็มปอดและทิวทัศน์อันสวยงามของเทือกเขา...</p>
                         </div>
 
                     </div>
@@ -253,7 +327,7 @@
 
             <!-- ปุ่มดูทั้งหมด -->
             <div class="col-md-auto" align="right">
-                <a type="button" class="myButton1" href="#">ดูทั้งหมด</a>
+                <a type="button" class="myButtonSeeAll" href="#">ดูทั้งหมด >></a>
             </div>
 
         </div>
@@ -272,12 +346,12 @@
 
                         <!-- รูปในการ์ดที่ 1 -->
                         <a href="#">
-                            <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./bangsaen.jpg" class="card-img-top" alt="...">
+                            <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./bangsaen2.jpg" class="card-img-top" alt="...">
                         </a>
                         <!-- ข้อความในการ์ดที่ 1 -->
                         <div class="card-body">
                             <a href="#" class="h2 text-decoration-none text-dark">บางแสน</a>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <p class="card-text">หาดบางแสนเป็นสถานที่ท่องเที่ยวที่เป็นที่รู้จักและนิยมมาอย่างยาวนานของนักท่องเที่ยว ด้วยความที่อยู่ใกล้กรุงเทพมหานคร ด้วยการเดินทางรถยนต์ใช้เวลาเพียงชั่วโมงเศษมีความยาวประมาณ...</p>
                         </div>
 
                     </div>
@@ -294,12 +368,12 @@
 
                                 <!-- รูปในการ์ดที่ 2 -->
                                 <a href="#">
-                                    <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./bangkok.jpg" class="card-img-top" alt="...">
+                                    <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./cejan.jpg" class="card-img-top" alt="...">
                                 </a>
                                 <!-- ข้อความในการ์ดที่ 2 -->
                                 <div class="card-body">
-                                    <a href="#" class="h2 text-decoration-none text-dark">กรุงเทพ</a>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                    <a href="#" class="h2 text-decoration-none text-dark">เขาชีจรรย์</a>
+                                    <p class="card-text">เขาชีจรรย์เป็นเขาหินปูนในนาจอมเทียนที่กลายเป็นสถานที่สำคัญของพัทยาเนื่องจากมี...</p>
                                 </div>
 
                             </div>
@@ -313,12 +387,12 @@
 
                                 <!-- รูปในการ์ดที่ 3 -->
                                 <a href="#">
-                                    <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./bangkok.jpg" class="card-img-top" alt="...">
+                                    <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./bangmong.jpg" class="card-img-top" alt="...">
                                 </a>
                                 <!-- ข้อความในการ์ดที่ 3 -->
                                 <div class="card-body">
-                                    <a href="#" class="h2 text-decoration-none text-dark">กรุงเทพ</a>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                    <a href="#" class="h2 text-decoration-none text-dark">บางละมง</a>
+                                    <p class="card-text">อำเภอบางละมุง เป็นเมืองท่องเที่ยวที่มีความสำคัญของจังหวัดชลบุรี ซึ่งรู้จักกันใน...</p>
                                 </div>
 
                             </div>
@@ -333,12 +407,12 @@
 
                                 <!-- รูปในการ์ดที่ 4 -->
                                 <a href="#">
-                                    <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./bangsaen.jpg" class="card-img-top" alt="...">
+                                    <img src="<?php echo base_url() . 'assets/templete/picture' ?>/./kaolan.jpg" class="card-img-top" alt="...">
                                 </a>
                                 <!-- ข้อความในการ์ดที่ 4 -->
                                 <div class="card-body">
-                                    <a href="#" class="h2 text-decoration-none text-dark">บางแสน</a>
-                                    <p class="card-text">Lorem ipsum dolor sit amet.</p>
+                                    <a href="#" class="h2 text-decoration-none text-dark">เกาะล้าน</a>
+                                    <p class="card-text">เกาะล้าน ตั้งอยู่ในเขตอำเภอบางละมุง จังหวัดชลบุรี...</p>
                                 </div>
 
                             </div>
@@ -357,7 +431,7 @@
                                 <!-- ข้อความในการ์ดที่ 5 -->
                                 <div class="card-body">
                                     <a href="#" class="h2 text-decoration-none text-dark">พัทยา</a>
-                                    <p class="card-text">Lorem ipsum dolor sit amet.</p>
+                                    <p class="card-text">พัทยามีชายหาดสวยงามเป็นที่รู้จักในหมู่นักท่องเที่ยว...</p>
                                 </div>
 
                             </div>
@@ -368,11 +442,23 @@
 
             <!-- ปุ่มดูทั้งหมด -->
             <div class="col-md-auto py-4" align="right">
-                <a type="button" class="myButton1" href="#">ดูทั้งหมด</a>
+                <a type="button" class="myButtonSeeAll" href="#">ดูทั้งหมด >></a>
             </div>
         </div>
         <!-- สิ้นสุดสถานที่ยอดนิยม -->
     </section>
     <!-- สิ้นสุดสีพื้นหลัง bg-light -->
 </body>
+
 </html>
+
+<script>
+    $(document).ready(function() {
+
+        $(document).on('click', '.myButton', function() {
+            $('.myButton').removeClass("active");
+            $(this).addClass("active");
+        });
+
+    });
+</script>
--- html/team2/application/views/template/Landing_page/topbar_landing.php
+++ html/team2/application/views/template/Landing_page/topbar_landing.php
@@ -4,14 +4,12 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top shadow">
         <div class="container d-flex justify-content-between align-items-center">
 
+            <!-- topbar left -->
             <a class="navbar-brand text-success logo h1 align-self-center" href="<?php echo base_url() . 'Landing_page/Register/Landing_page' ?>">
                 Logo
             </a>
 
-            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
+            <!-- topbar right -->
             <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
                 <div class="flex-fill">
                     <ul class="nav d-flex justify-content-end mx-lg-auto">
@@ -27,6 +25,7 @@
                     </ul>
                 </div>
             </div>
+            
         </div>
     </nav>
     <!-- Close topbar -->
