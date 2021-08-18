80ee5bab37d1f07e226677ba82f0066598340212
Naaka Punparich <62160082@go.buu.ac.th>
2021-08-18 15:23:00 +0700

    edit topbar landing page

--- html/team2/application/views/template/Landing_page/topbar_landing.php
+++ html/team2/application/views/template/Landing_page/topbar_landing.php
@@ -1,35 +1,34 @@
 <!-- Make by : Naaka Punparich 62160082 -->
 
-<body>
-    <!-- Open topbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top shadow">
+<!-- Open topbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top shadow">
 
-        <div class="container d-flex justify-content-between align-items-center">
-            <!-- topbar left -->
-            <a href="#" class="navbar-brand">
-                <img src="<?php echo base_url() . 'assets/templete/picture/./2-3.png' ?>" style="max-width:250px; height: 100px; margin-top: -20px; background: transparent;">
-            </a>
+    <div class="container-fluid" style="margin-bottom: -2%;">
+        <!-- topbar left -->
+        <a href="#" class="navbar-brand">
+            <img src="<?php echo base_url() . 'assets/templete/picture/./2-3.png' ?>" style="max-width:300px; height: 150px; margin-top: -45px;">
+        </a>
 
-            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <!-- topbar right -->
-            <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
-                <div class="flex-fill">
-                    <ul class="nav d-flex justify-content-end mx-lg-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url() . 'Entrepreneur/Auth/Login_entrepreneur' ?>">ผู้ประกอบการ</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url() . 'Tourist/Auth/Login_tourist' ?>">เข้าสู่ระบบ</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url() . 'Landing_page/Register/Select_register' ?>">สมัครสมาชิก</a>
-                        </li>
-                    </ul>
-                </div>
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- topbar right -->
+        <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
+            <div class="flex-fill">
+                <ul class="nav d-flex justify-content-end mx-lg-auto" style="margin-top: -40px;">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url() . 'Entrepreneur/Auth/Login_entrepreneur' ?>">ผู้ประกอบการ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url() . 'Tourist/Auth/Login_tourist' ?>">เข้าสู่ระบบ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url() . 'Landing_page/Register/Select_register' ?>">สมัครสมาชิก</a>
+                    </li>
+                </ul>
             </div>
-
         </div>
-    </nav>
-    <!-- Close topbar -->
+
+    </div>
+</nav>
+<!-- Close topbar -->
