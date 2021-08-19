e08da13b24230919248b76414923656512c18b71
Naaka Punparich <62160082@go.buu.ac.th>
2021-08-19 19:08:26 +0700

    fix

--- html/team2/application/controllers/Tourist/Event_tourist/Tourist_event.php
+++ html/team2/application/controllers/Tourist/Event_tourist/Tourist_event.php
@@ -6,11 +6,21 @@ class Tourist_event extends DCS_controller
 {
     public function show_tourist_eventlist()
     {
-        $this->output_event('tourist/event_tourist/v_eventlist_tourist');
+        $this->output_event('tourist/event_tourist/v_list_event_tourist','template/Tourist/topbar_tourist');
     }
 
     public function show_detailevent_tourist()
     {
-        $this->output_event('tourist/event_tourist/v_detailevent_tourist');
+        $this->output_event('tourist/event_tourist/v_detail_event_tourist','template/Tourist/topbar_tourist');
+    }
+
+    public function show_tourist_eventlist_login()
+    {
+        $this->output_event('tourist/event_tourist/v_list_event_tourist','template/Tourist/topbar_tourist_login');
+    }
+
+    public function show_detailevent_tourist_login()
+    {
+        $this->output_event('tourist/event_tourist/v_detail_event_tourist','template/Tourist/topbar_tourist_login');
     }
 }
