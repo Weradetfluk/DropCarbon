f34cad565ab5af0ed4f8e9065a871f8c3c0ebb73
Thanisorn <62160088@go.buu.ac.th>
2021-08-20 23:18:03 +0700

    comment Regis tourist

--- html/team2/application/controllers/Landing_page/Register/Select_register.php
+++ html/team2/application/controllers/Landing_page/Register/Select_register.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 include_once dirname(__FILE__) . '/../../DCS_controller.php';
 /*
 * Select_register
+* show register page
 * @author Thanisorn thumsawanit 62160088
 * @Create Date 2564-07-19
 */
