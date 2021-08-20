11d1e93b72a2dbdc3dee3569b8b683d5259c20e1
Thanisorn <62160088@go.buu.ac.th>
2021-08-20 21:19:13 +0700

    comment file

--- html/team2/application/controllers/Landing_page/Register/Select_register.php
+++ html/team2/application/controllers/Landing_page/Register/Select_register.php
@@ -1,7 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 include_once dirname(__FILE__) . '/../../DCS_controller.php';
+/*
+* Select_register
+* @author Thanisorn thumsawanit 62160088
+* @Create Date 2564-07-19
+*/
 class Select_register extends DCS_controller {
+    /*
+    * index
+    * output Select_register page
+    * @input
+    * @output -
+    * @author Thanisorn thumsawanit 62160088
+    * @Create Date 2564-07-19
+    * @Update -
+    */
     public function index() {
         $this->output_regis('landing_page/register/v_regis');
     }
