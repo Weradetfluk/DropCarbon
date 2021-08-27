8cffde424a46a4d99d3f76e7ed0f4988b3bec142
Naaka Punparich <62160082@go.buu.ac.th>
2021-08-27 14:00:23 +0700

    check file_name null

--- html/team2/application/controllers/Tourist/Manage_tourist/Tourist_edit.php
+++ html/team2/application/controllers/Tourist/Manage_tourist/Tourist_edit.php
@@ -54,46 +54,51 @@ class Tourist_edit extends DCS_controller
       $tus_pre_id = $this->mtou->tus_pre_id;
       $this->session->set_userdata("pre_id", $tus_pre_id);
 
-      // Create file storage variables
-      $file_name = array();
-      $file_tmp_name = array();
-      $file_size = array();
-      $file_error = array();
-      $file_ext = array();
-      $file_actaul_ext = array();
-      $error_file = '';
+      if (isset($_FILES["tourist_img"]) && !empty($_FILES["tourist_img"]["name"])) {
+         // Create file storage variables
+         $file_name = array();
+         $file_tmp_name = array();
+         $file_size = array();
+         $file_error = array();
+         $file_ext = array();
+         $file_actaul_ext = array();
+         $error_file = '';
 
-      // Configure file storage
-      $file = $_FILES['tourist_img'];
-      $file_name = $_FILES['tourist_img']['name'];
-      $file_tmp_name = $_FILES['tourist_img']['tmp_name'];
-      $file_size = $_FILES['tourist_img']['size'];
-      $file_error = $_FILES['tourist_img']['error'];
+         // Configure file storage
+         $file = $_FILES['tourist_img'];
+         $file_name = $_FILES['tourist_img']['name'];
+         $file_tmp_name = $_FILES['tourist_img']['tmp_name'];
+         $file_size = $_FILES['tourist_img']['size'];
+         $file_error = $_FILES['tourist_img']['error'];
 
-      $file_ext = explode('.', $file_name);
-      $file_actaul_ext = strtolower(end($file_ext));
+         $file_ext = explode('.', $file_name);
+         $file_actaul_ext = strtolower(end($file_ext));
 
-      // Check if there is a problem with the image file. or the file size exceeds 1000000?
-      if ($file_error != 0 || $file_size >= 3000000) {
-         $error_file = 'false';
-      }// ต้องมาแก้ขนาดอีกที  ปรึกษาเรื่องขนาดกับ PO
+         // Check if there is a problem with the image file. or the file size exceeds 1000000?
+         if ($file_error != 0 || $file_size >= 3000000) {
+            $error_file = 'false';
+         } // ต้องมาแก้ขนาดอีกที  ปรึกษาเรื่องขนาดกับ PO
 
-      if ($error_file != 'false') {
+         if ($error_file != 'false') {
+            $this->mtou->update_tourist();
+            $this->mpic->tus_img_tus_id = $tus_id;
+            $this->mpic->delete_img_by_id($tus_id);
+            // Loop to upload files
+            $file_new_name = uniqid('', true);
+            $file_destination = './profilepicture_tourist/' . $file_new_name . '.' . $file_actaul_ext;
+            move_uploaded_file($file_tmp_name, $file_destination);
+            $this->mpic->tus_img_path = $file_new_name . '.' . $file_actaul_ext;
+            $tus_img_path = $this->mpic->tus_img_path;
+            $this->session->set_userdata("tus_img_path", $tus_img_path);
+            $this->mpic->insert_img();
+
+            redirect("Tourist/Auth/Landing_page_tourist");
+         } else {
+            $this->show_edit_tourist();
+         }
+      } else {
          $this->mtou->update_tourist();
-         $this->mpic->tus_img_tus_id = $tus_id;
-         $this->mpic->delete_img_by_id($tus_id);
-         // Loop to upload files
-         $file_new_name = uniqid('', true);
-         $file_destination = './profilepicture_tourist/' . $file_new_name . '.' . $file_actaul_ext;
-         move_uploaded_file($file_tmp_name, $file_destination);
-         $this->mpic->tus_img_path = $file_new_name . '.' . $file_actaul_ext;
-         $tus_img_path = $this->mpic->tus_img_path;
-         $this->session->set_userdata("tus_img_path", $tus_img_path);
-         $this->mpic->insert_img();
-         
          redirect("Tourist/Auth/Landing_page_tourist");
-      }else {
-         $this->show_edit_tourist();
       }
    }
 }
