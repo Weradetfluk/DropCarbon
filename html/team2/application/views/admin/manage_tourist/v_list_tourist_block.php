463e6c1705cb587df72fb514fcd546555b21f885
AemAns <72913787+AemAns@users.noreply.github.com>
2021-08-28 14:27:09 +0700

    edit search bar

--- html/team2/application/views/admin/manage_tourist/v_list_tourist_block.php
+++ html/team2/application/views/admin/manage_tourist/v_list_tourist_block.php
@@ -41,15 +41,15 @@
                                  
                                <div class="row">
                                      <div class="col py-2">
-                                         <h4 class="card-title text-white ">ตารางแสดงข้อมูลบัญชีของนักท่องเที่ยวที่ถูกบล็อค</h4>
+                                         <h4 class="card-title text-white ">ตารางแสดงบัญชีของนักท่องเที่ยวที่ถูกบล็อค</h4>
                                      </div>
                                      <div class="col">
-                                         <form class="form-inline" action="#" method="POST" style="width: 200px; float:right;">
+                                         <form class="form-inline custom-form-search" action="#">
 
                                              <div class="input-group ">
 
-                                                 <input type="text" value="" name="value_search" class="form-control" placeholder="  ค้นหาชื่อได้ที่นี่..." style="background-color:white; border-radius: 10px;">
-                                                 <button type="submit" name="search" class="btn btn-white btn-round btn-just-icon" value="">
+                                             <input type="text" value="" name="value_search" class="form-control custom-search" placeholder="  ค้นหาชื่อได้ที่นี่...">
+                                                 <button type="submit" name="search" class="btn btn-white btn-round btn-just-icon" value=""  style="margin-left: 3px;">
                                                      <i class="material-icons">search</i>
                                                  </button>
 
