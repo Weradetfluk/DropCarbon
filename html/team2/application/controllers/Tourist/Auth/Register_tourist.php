eba72302a4bfaf6bbba1db1de6f1ab9fc2a8379c
Naaka Punparich <62160082@go.buu.ac.th>
2021-09-04 01:52:04 +0700

    fix photo register tourist

--- html/team2/application/controllers/Tourist/Auth/Register_tourist.php
+++ html/team2/application/controllers/Tourist/Auth/Register_tourist.php
@@ -51,45 +51,59 @@ class Register_tourist extends DCS_controller
         $this->mtou->tus_password = $this->input->post('tus_password');
         $this->mtou->tus_status = 1;
 
-        if (isset($_FILES["tourist_img"]) && !empty($_FILES["tourist_img"]["name"])) {
-            // Create file storage variables
-            $file_name = array();
-            $file_tmp_name = array();
-            $file_size = array();
-            $file_error = array();
-            $file_ext = array();
-            $file_actaul_ext = array();
-            $error_file = '';
+        // if (isset($_FILES["tourist_img"]) && !empty($_FILES["tourist_img"]["name"])) {
+        // Create file storage variables
+        $file_name = array();
+        $file_tmp_name = array();
+        $file_size = array();
+        $file_error = array();
+        $file_ext = array();
+        $file_actaul_ext = array();
+        $error_file = '';
 
-            // Configure file storage
-            $file = $_FILES['tourist_img'] ?? '';
-            $file_name = $_FILES['tourist_img']['name'] ?? '';
-            $file_tmp_name = $_FILES['tourist_img']['tmp_name'] ?? '';
-            $file_size = $_FILES['tourist_img']['size'] ?? '';
-            $file_error = $_FILES['tourist_img']['error'] ?? '';
+        // Configure file storage
+        $file = $_FILES['tourist_img'] ?? '';
+        $file_name = $_FILES['tourist_img']['name'] ?? '';
+        $file_tmp_name = $_FILES['tourist_img']['tmp_name'] ?? '';
+        $file_size = $_FILES['tourist_img']['size'] ?? '';
+        $file_error = $_FILES['tourist_img']['error'] ?? '';
 
-            // Check if there is a problem with the image file. or the file size exceeds 1000000?
+        if ($file != '') {
+            $file_ext = explode('.', $file_name);
+            $file_actaul_ext = strtolower(end($file_ext));
+
             if ($file_error != 0 || $file_size >= 3000000) {
                 $error_file = 'false';
             }
-
-            if ($error_file != 'false') {
-                $this->mtou->insert_tourist();
-                $result = $this->mtou->get_by_username_password();
-                $this->mpic->tus_img_tus_id = $result->tus_id;
-                // Loop to upload files
-                $file_new_name = uniqid('', true);
-                $file_destination = './profilepicture_tourist/' . $file_new_name . '.' . $file_actaul_ext;
-                move_uploaded_file($file_tmp_name, $file_destination);
-                $this->mpic->tus_img_path = $file_new_name . '.' . $file_actaul_ext;
-                $this->mpic->insert_img();
-
-                redirect('Landing_page/Register/Landing_page'); //redirect ไปที่หน้าหลัก
-            }
         } else {
+            $error_file = 'false';
+        }
+        // Check if there is a problem with the image file. or the file size exceeds 1000000?
+
+        if ($error_file != 'false') {
+            $this->mtou->insert_tourist();
+            $result = $this->mtou->get_by_username_password();
+            $this->mpic->tus_img_tus_id = $result->tus_id;
+            // Loop to upload files
+            $file_new_name = uniqid('', true);
+            $file_destination = './profilepicture_tourist/' . $file_new_name . '.' . $file_actaul_ext;
+            move_uploaded_file($file_tmp_name, $file_destination);
+            $this->mpic->tus_img_path = $file_new_name . '.' . $file_actaul_ext;
+            $this->mpic->insert_img();
+            $this->set_session_regis_tourist('success');
+            redirect('Landing_page/Register/Landing_page'); //redirect ไปที่หน้าหลัก
+        } else if (isset($_FILES["tourist_img"]) && empty($_FILES["tourist_img"]["name"])) {
+            $this->set_session_regis_tourist('success');
             $this->mtou->insert_tourist();
             redirect('Landing_page/Register/Landing_page');
+        } else {
+            $this->set_session_regis_tourist('fail');
+            redirect('Tourist/Auth/Register_tourist/show_regis_tourist');
         }
+        // } else {
+        //     $this->mtou->insert_tourist();
+        //     redirect('Landing_page/Register/Landing_page');
+        // }
     }
 
     /*
