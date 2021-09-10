6d10f9231ccc5ae06987c758f30667df3e9c4b9f
Chutipon Thermsirisuksin <62160081@go.buu.ac.th>
2021-09-10 23:09:54 +0700

    Add required in tag input username and password for tourist by Chutipon 10/09/64

--- html/team2/application/views/tourist/auth/v_login_tourist.php
+++ html/team2/application/views/tourist/auth/v_login_tourist.php
@@ -5,14 +5,14 @@ $warning = $warning ?? ''; //check world warnning == username หรือ passw
 <div class="body">
     <div class="container-1">
         <div class="brand-logo">
-        <i class="fas fa-9x fa-user-circle" style="color: #59ab6e;"></i>
+            <i class="fas fa-9x fa-user-circle" style="color: #59ab6e;"></i>
         </div>
         <form method="post" action="<?php echo site_url() . 'Tourist/Auth/Login_tourist/input_login_form'; ?>">
             <div class="inputs">
                 <label class="label">USERNAME</label>
-                <input class="input" type="text" placeholder="ชื่อผู้ใช้" name="username">
+                <input class="input" type="text" placeholder="ชื่อผู้ใช้" name="username" required>
                 <label class="label">PASSWORD</label>
-                <input class="input" type="password" placeholder="รหัสผ่าน" name="password">
+                <input class="input" type="password" placeholder="รหัสผ่าน" name="password" required>
                 <span style="color: red; margin-left: 10px;">
                     <?php
                     if ($warning != NULL) {
