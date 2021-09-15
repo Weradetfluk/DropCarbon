f944ac5125ca95ea3344a2f523c0f360f4e082c6
Naaka Punparich <62160082@go.buu.ac.th>
2021-09-15 11:23:58 +0700

    fix topbar tourist , tourist login

--- html/team2/application/views/template/Tourist/topbar_tourist.php
+++ html/team2/application/views/template/Tourist/topbar_tourist.php
@@ -1,9 +1,20 @@
 <!-- Make by : Naaka Punparich 62160082 -->
 
 <style>
+    .container-fluid {
+        margin-top: 8px;
+        margin-left: 20px;
+    }
+
     #buttonfix {
         width: unset;
         box-shadow: unset;
+    }
+
+    @media screen and (max-width: 400px) {
+        .container-fluid {
+            margin-left: 1px;
+        }
     }
 </style>
 
@@ -11,7 +22,7 @@
 
 <!-- Open topbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top shadow">
-    <div class="container-fluid" style="margin-top: 8px; margin-left: 20px;">
+    <div class="container-fluid">
         <!-- topbar left -->
         <a class="navbar-brand" href="<?php echo site_url() . 'Landing_page/Register/Landing_page'; ?>">
             <img src="<?php echo base_url() . 'assets/templete/picture/./Logo-web.png' ?>" style="max-width:400px; height: 50px; margin-top: -10px; margin-left: -40px;">
