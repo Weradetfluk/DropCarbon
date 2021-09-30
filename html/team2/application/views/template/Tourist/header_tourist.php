c4e6a74b7b46a4fdc35ca477630695a934ce65ee
Naaka Punparich <62160082@go.buu.ac.th>
2021-09-30 12:54:08 +0700

    change landing page

--- html/team2/application/views/template/Tourist/header_tourist.php
+++ html/team2/application/views/template/Tourist/header_tourist.php
@@ -13,25 +13,21 @@
 
 
     <!-- template css -->
-    <!-- <link rel="stylesheet" href="<?php echo base_url() . 'assets/templete/material-dashboard-master' ?>/assets/css/material-dashboard.min.css?v=2.1.2" type="text/css"> -->
-
     <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url() . 'assets/templete/material-dashboard-master' ?>/assets/css/material-dashboard.min.css?v=2.1.2" as="style" type="text/css">
     <!-- custom css -->
-    <!-- <link rel="stylesheet" href="<?php echo base_url() . 'assets/templete/material-dashboard-master' ?>/assets/css/custom-admin.css" type="text/css"> -->
-
     <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url() . 'assets/templete/material-dashboard-master' ?>/assets/css/custom.css" as="style" type="text/css">
     <!-- fonts css -->
     <link rel="stylesheet" href="<?php echo base_url() . 'assets/templete/material-dashboard-master' ?>\assets\fonts\fonts_prompt\font_prompt.css" type="text/css">
-    <!-- <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url() . 'assets/templete/material-dashboard-master' ?>/assets/css/fonts.css" as="style" type="text/css"> -->
-    <!-- <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>\assets\fonts\fonts_prompt\font_prompt.css" type="text/css"> -->
 
     <!-- sweet alert plugin -->
-    <!-- <link rel="stylesheet" href="<?php echo base_url() . 'assets/plugin/sweetalert/sweetalert.min.css' ?>" type="text/css"> -->
     <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url() . 'assets/plugin/sweetalert/sweetalert.min.css' ?>" as="style" type="text/css">
     <!-- material icon css -->
     <link rel="stylesheet" href="<?php echo base_url() . 'assets/templete' ?>/material_icon.css" type="text/css">
     <link rel="stylesheet" href="<?php echo base_url() . 'assets/plugin/slick/slick.css' ?>">
     <link rel="stylesheet" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>\assets\fonts\fonts_prompt\font_prompt.css" type="text/css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <style>
         body {
             font-size: 18px;
