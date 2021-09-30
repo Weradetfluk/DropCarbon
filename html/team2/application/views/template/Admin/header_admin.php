1d1a78f36b1f86f0ded9126676f4706605352798
weradet <fluk.weradet@gmail.com>
2021-09-30 11:03:58 +0700

    header-admin 62160110

--- html/team2/application/views/template/Admin/header_admin.php
+++ html/team2/application/views/template/Admin/header_admin.php
@@ -15,22 +15,24 @@
 
 
     <!-- template css -->
-    <!-- <link rel="stylesheet" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/material-dashboard.min.css?v=2.1.2" type="text/css"> -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/material-dashboard.min.css?v=2.1.2" type="text/css">
 
-    <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/material-dashboard.min.css?v=2.1.2" as="style" type="text/css">
+    <!-- <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/material-dashboard.min.css?v=2.1.2" as="style" type="text/css"> -->
+  
+  
     <!-- custom css -->
-    <!-- <link rel="stylesheet" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/custom-admin.css" type="text/css"> -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/custom-admin.css" type="text/css">
 
-    <link rel="preload" onload="this.rel = 'stylesheet'"href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/custom-admin.css" as="style" type="text/css">
+    <!-- <link rel="preload" onload="this.rel = 'stylesheet'"href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/custom-admin.css" as="style" type="text/css"> -->
     <!-- fonts css -->
-    <!-- <link rel="stylesheet" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/fonts.css" type="text/css"> -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/fonts.css" type="text/css">
     <!-- <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>/assets/css/fonts.css" as="style" type="text/css"> -->
-    <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>\assets\fonts\fonts_prompt\font_prompt.css" type="text/css">
-
      <!-- sweet alert plugin -->
-    <!-- <link rel="stylesheet" href="<?php echo base_url().'assets/plugin/sweetalert/sweetalert.min.css'?>" type="text/css"> -->
-    <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url().'assets/plugin/sweetalert/sweetalert.min.css'?>" as="style" type="text/css">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/plugin/sweetalert/sweetalert.min.css'?>" type="text/css">
+    <!-- <link rel="preload" onload="this.rel = 'stylesheet'" href="<?php echo base_url().'assets/plugin/sweetalert/sweetalert.min.css'?>" as="style" type="text/css"> -->
     <!-- material icon css -->
     <link rel="stylesheet" href="<?php echo base_url().'assets/templete'?>/material_icon.css" type="text/css">
+        <link rel="stylesheet" href="<?php echo base_url().'assets/templete/material-dashboard-master'?>\assets\fonts\fonts_prompt\font_prompt.css" type="text/css">	
+
 </head>
 <body>
