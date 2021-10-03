444a7efa137d4c2a6cb933915d6e33d24c72f2ba
Naaka Punparich <62160082@go.buu.ac.th>
2021-10-03 13:05:25 +0700

    footer tourist

---
+++ html/team2/application/views/template/Tourist/footer.php
@@ -0,0 +1,37 @@
+<footer>
+    <div class="footer-top" style="padding: 50px 0 40px 0;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3>Drop Carbon System</h3>
+                    <p>การท่องเที่ยวเชิงอนุรักษ์สิ่งแวดล้อม</p>
+                    <p>© Team2 Barapha University</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>ติดต่อเรา</h3>
+                    <p><i class="fas fa-map-marker-alt"></i> 24/2 ม.1 ต.แสนสุข อ.เมือง จ.ชลบุรี</p>
+                    <p><i class="fas fa-envelope"></i> อีเมล: <a href="#">dropcarbonsystem@gmail.com</a></p>
+                    <p><i class="fas fa-phone"></i> เบอร์ติดต่อ: 085-2812191</p>
+                </div>
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="col">
+                            <h3>ทางลัด</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p><a href="#">เข้าสู่ระบบ</a></p>
+                            <p><a href="#">สถานที่ท่องเที่ยว</a></p>
+                            <p><a href="#">กิจกรรม</a></p>
+                        </div>
+                        <div class="col-md-6">
+                            <p><a href="#">ผู้ประกอบการ</a></p>
+                            <p><a href="#">ผู้ดูแลระบบ</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
