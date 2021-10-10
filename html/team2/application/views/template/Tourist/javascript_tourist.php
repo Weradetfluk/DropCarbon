ead0645a152b1f6cc583ffbcfd363d0ff3e1e594
Naaka Punparich <62160082@go.buu.ac.th>
2021-10-10 14:39:57 +0700

    delay js

--- html/team2/application/views/template/Tourist/javascript_tourist.php
+++ html/team2/application/views/template/Tourist/javascript_tourist.php
@@ -19,7 +19,7 @@
  <script>
      AOS.init({
          offset: 120, // offset (in px) from the original trigger point
-         delay: 250, // values from 0 to 3000, with step 50ms
+         delay: 50, // values from 0 to 3000, with step 50ms
          duration: 600, // values from 0 to 3000, with step 50ms
          easing: 'ease', // default easing for AOS animations
          once: false, // whether animation should happen only once - while scrolling down
