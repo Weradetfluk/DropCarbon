10795fdca2be43997a259879d5e1b2e7677b7115
Jutamas Thaptong <62160079@go.buu.ac.th>
2021-11-18 17:05:59 +0700

    edit css in file company

--- html/team2/application/views/landing_page/v_list_company.php
+++ html/team2/application/views/landing_page/v_list_company.php
@@ -8,26 +8,15 @@
 * @Create Date 2564-09-14
 */ 
 -->
-<style>
-.card-custom {
-    border-radius: 20px;
-}
 
-.card-img-top {
-    border-top-left-radius: 20px;
-    border-top-right-radius: 20px;
-    height: 300px;
-    object-fit: cover;
-}
-</style>
 <div class="container py-5 section-com">
     <ul class="breadcrumb">
         <?php if ($this->session->userdata("tourist_id")) { ?>
-        <li><a href="<?php echo base_url() . 'Tourist/Auth/Landing_page_tourist' ?>" style="color: green;">หน้าหลัก</a>
-        </li>
+            <li><a href="<?php echo base_url() . 'Tourist/Auth/Landing_page_tourist' ?>" style="color: green;">หน้าหลัก</a>
+            </li>
         <?php } ?>
         <?php if (!$this->session->userdata("tourist_id")) { ?>
-        <li><a href="<?php echo base_url() ?>" style="color: green;">หน้าหลัก</a></li>
+            <li><a href="<?php echo base_url() ?>" style="color: green;">หน้าหลัก</a></li>
         <?php } ?>
         <li class="colorchange">ดูรายการสถานที่</li>
     </ul>
@@ -43,17 +32,16 @@
     <form action="<?= site_url() ?>Landing_page/Landing_page/show_company_list" class="" method="post">
         <div class="row justify-content-md-center" style="margin: 0px 0px 30px 0px;">
             <div class="col-md-4">
-                <input type="text" value="<?= $value_search ?>" id="search_box" name="value_search"
-                    class="form-control form-control-custom" placeholder="ค้นหาสถานที่">
+                <input type="text" value="<?= $value_search ?>" id="search_box" name="value_search" class="form-control form-control-custom" placeholder="ค้นหาสถานที่">
             </div>
             <div class="col-md-2">
                 <!-- <select class="form-control form-control-custom" name="txt_category"> -->
                 <select name="com_cat_id" id="com_cat_id" class="form-control form-control-custom">
                     <option value="">เลือกทั้งหมด</option>
                     <?php for ($i = 0; $i < count($com_cat); $i++) { ?>
-                    <?php $selected = $_POST["com_cat_id"] == $com_cat[$i]->com_cat_id ? "selected" : ""; ?>
-                    <option value="<?php echo $com_cat[$i]->com_cat_id ?>" <?= $selected ?>>
-                        <?php echo $com_cat[$i]->com_cat_name; ?></option>
+                        <?php $selected = $_POST["com_cat_id"] == $com_cat[$i]->com_cat_id ? "selected" : ""; ?>
+                        <option value="<?php echo $com_cat[$i]->com_cat_id ?>" <?= $selected ?>>
+                            <?php echo $com_cat[$i]->com_cat_name; ?></option>
                     <?php } ?>
                 </select>
                 <!-- </select> -->
@@ -67,23 +55,21 @@
     <div class="container">
         <div class="row py-3">
             <?php for ($i = 0; $i < count($company); $i++) { ?>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-4 py-2">
-                <a
-                    href="<?php echo base_url() . 'Landing_page/Landing_page/show_company_detail/' . $company[$i]->com_id; ?>">
-                    <div class="card card-custom" style="height: 30rem;" id="card">
-                        <img src="<?php echo base_url() . 'image_company/' . $company[$i]->com_img_path; ?>"
-                            class="card-img-top" style="height: 300px; object-fit: cover;" alt="...">
-                        <!-- รูปที่ 1 -->
-                        <div class="card-body" align="center">
-                            <h3 class="text-decoration-none text-dark"><?php echo $company[$i]->com_name ?></h3>
-                            <p class="card-tex text-dark">
-                                <?php echo iconv_substr($company[$i]->com_description, 0, 60, "UTF-8") . "..."; ?>
-                            </p>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-4 py-2">
+                    <a href="<?php echo base_url() . 'Landing_page/Landing_page/show_company_detail/' . $company[$i]->com_id; ?>">
+                        <div class="card card-custom" style="height: 30rem;" id="card">
+                            <img src="<?php echo base_url() . 'image_company/' . $company[$i]->com_img_path; ?>" class="card-img-top" style="height: 300px; object-fit: cover;" alt="...">
+                            <!-- รูปที่ 1 -->
+                            <div class="card-body" align="center">
+                                <h3 class="text-decoration-none text-dark"><?php echo $company[$i]->com_name ?></h3>
+                                <p class="card-tex text-dark">
+                                    <?php echo iconv_substr($company[$i]->com_description, 0, 60, "UTF-8") . "..."; ?>
+                                </p>
+                            </div>
+                            <!-- ชื่อของรูปที่ 1 -->
                         </div>
-                        <!-- ชื่อของรูปที่ 1 -->
-                    </div>
-                </a>
-            </div>
+                    </a>
+                </div>
             <?php } ?>
         </div>
     </div>
